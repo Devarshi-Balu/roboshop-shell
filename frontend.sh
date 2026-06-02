2b731b7cd4233d20b4988f39f510aaf81a376710
devarshi-balu <studious.devarshibalu@gmail.com>
2026-06-02 12:30:43 +0530

    add frontend config,script files

--- frontend.sh
+++ frontend.sh
@@ -31,3 +31,35 @@ function validate(){
         echo -e "$1 ... $G Success $N";
     fi
 }
+
+dnf module disable nginx -y
+validate "disabling nginx module"
+
+dnf module enable nginx:1.24 -y
+validate "enabling nginx 1.24 version"
+
+dnf install nginx -y 
+validate "installing nginx package"
+
+systemctl enable nginx 
+systemctl start nginx 
+validate "enabling and starting nginx"
+
+rm -rf "/usr/share/nginx/html/*"
+validate "removing existing html file in /usr/share/nginx/html/*.html"
+
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip
+validate "downloading the frontend code files zip folder"
+
+(
+    set -e 
+    cd /usr/share/nginx/html 
+    unzip /tmp/frontend.zip
+)
+validate "unzipping the code files"
+
+cp "${script_path_dir}/nginx.conf" "etc/nginx/nginx.conf"
+validate "copying the nginx config file"
+
+systemctl restart nginx 
+validate "restarting nginx module"
